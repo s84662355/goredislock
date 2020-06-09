bede3b4b6602eedec62c63eebbf468b302bbb615
chenjiahao <271375247@qq.com>
2020-06-10 00:55:09 +0800

    a

--- script/wlock.go
+++ script/wlock.go
@@ -23,42 +23,12 @@ var wlock string = `
 		local client_name =  ARGV[2]
 		local a = redis.call('TIME')
 		local cur_timestamp =  tonumber( a[1]  )
-		local result=0
 		local lockdata =  expire + cur_timestamp
 		lockdata = lockdata.. "???" .. client_name
-		result = redis.call('setnx',key, lockdata)
+		 
 
-		if result == 0 then
+		return  redis.call('set',key, lockdata,'nx','ex',expire)
 
-			local keydata = string.split(redis.call('get',key),'???')
-			local time_out = tonumber(keydata[1])
-			local data_client_name =  keydata[2]
-
-			if cur_timestamp >  time_out then
-				time_out = expire + cur_timestamp
-				lockdata =  time_out .. "???" .. client_name
-
-				if redis.call('setex',key,expire,lockdata)   then
-					return    1
-
-				end
-
-				return 0
-			end
-
-			if data_client_name == client_name then
-
-				return 1
-			end
-
-			return 0
-		end
-
-		if  redis.call('Expire',key,expire)  then
-			return 1
-		end
-
-		return 0
 `
 
 
